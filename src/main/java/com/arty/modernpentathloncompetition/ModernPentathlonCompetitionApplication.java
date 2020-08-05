97c24468cf462012e5032c3addf3d0a93919c9d0
arturas.sisakovas@valuetech.lt <iamveryhappY1997>
2020-08-05 06:48:17 +0300

    Initial project setup

---
+++ src/main/java/com/arty/modernpentathloncompetition/ModernPentathlonCompetitionApplication.java
@@ -0,0 +1,13 @@
+package com.arty.modernpentathloncompetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernPentathlonCompetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernPentathlonCompetitionApplication.class, args);
+	}
+
+}
